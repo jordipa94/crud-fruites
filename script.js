f608a128d44e85c46de2e81c315272e80addfdb4
jordipa94 <jpujol6@inscaparrella.cat>
2024-10-22 16:11:13 +0200

    js agregar i mostrar fruites

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
 let fruites = [];
 
 function mostrarFruites() {
-
-
-
+    const llistaFruites = document.getElementById("llistaFruites");
+    llistaFruites.innerHTML = "";
+    if (fruites.length === 0) {
+        llistaFruites.innerHTML = "<li>No hi ha fruites.</li>";
+    } else {
+        fruites.forEach((fruita, index) => {
+            llistaFruites.innerHTML += `<li>${index + 1}. ${fruita}</li>`; //IMPRIMEIX NUMERO INDEX I LA FRUITA
+        });
+    }
 }
 
 function agregarFruita() {
-
-
-
+    const novaFruita = document.getElementById("novaFruita").value;
+    if (novaFruita) {
+        fruites.push(novaFruita); //AFEGEIX LA FRUITA NOVA AL ARRAY FRUITES[]
+        document.getElementById("novaFruita").value = "";
+        mostrarFruites(); // CRIDA LA FUNCIO MOSTRARFRUITES AUTOMATICAMENT
+    } else {
+        alert("Introdueix una fruita.");
+    }
 }
 
 function actualitzarFruita() {
