cb48f84e711d720f87e4a7e18f658da23b5ea859
jordipa94 <jpujol6@inscaparrella.cat>
2024-10-23 11:43:14 +0200

    js eliminar

--- script.js
+++ script.js
@@ -3,9 +3,10 @@ let fruites = [];
 function mostrarFruites() {
 
     const llistaFruites = document.getElementById("llistaFruites");
-    llistaFruites.innerHTML = "";
+
+    llistaFruites.innerHTML = ""; //LLIMPIAR INPUT
     if (fruites.length === 0) {
-        llistaFruites.innerHTML = "<li>No hi ha fruites.</li>";
+        llistaFruites.innerHTML = "<li>No hi ha fruites.</li>"; // ( NOMES SURT DESPRES DE ACTUALITZAR )
     } else {
         fruites.forEach((fruita, index) => {
             llistaFruites.innerHTML += `<li>${index + 1}. ${fruita}</li>`; //IMPRIMEIX NUMERO INDEX I LA FRUITA
@@ -13,12 +14,13 @@ function mostrarFruites() {
     }
 }
 
-function agregarFruita() {
+function AfegirFruita() {
 
     const novaFruita = document.getElementById("novaFruita").value;
+
     if (novaFruita) {
         fruites.push(novaFruita); //AFEGEIX LA FRUITA NOVA AL ARRAY FRUITES[]
-        document.getElementById("novaFruita").value = "";
+        document.getElementById("novaFruita").value = ""; //LLIMPIAR INPUT
         mostrarFruites(); // CRIDA LA FUNCIO MOSTRARFRUITES AUTOMATICAMENT
     } else {
         alert("Introdueix una fruita.");
@@ -33,6 +35,14 @@ function actualitzarFruita() {
 
 function eliminarFruita() {
 
+    const numEliminar = parseInt(document.getElementById("indexEliminar").value) - 1;
 
+    if (numEliminar >= 0 && numEliminar < fruites.length) {
+        fruites.splice(numEliminar, 1);
+        mostrarFruites();
+        document.getElementById("indexEliminar").value = ""; // LLIMPIAR INPUT 
+    } else {
+        alert("El index introduit no existeix");
+    }
 
 }
